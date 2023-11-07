2823fcc56281dceb7871c0081b9c662ed5e2121a
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-07 13:02:33 -0800

    activity complete

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,39 +4,74 @@ import Panel from "./Panel";
 
 import classnames from "classnames";
 
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads,
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10,
+    value: getTotalPhotos,
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4,
+    value: getTotalTopics,
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng",
+    value: getUserWithMostUploads,
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza",
+    value: getUserWithLeastUploads,
   },
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    photos: [],
+    topics: [],
   };
 
   selectPanel(id) {
     this.setState((previousState) => ({
       focused: previousState.focused !== null ? null : id,
     }));
+  }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    const urlsPromise = ["/api/photos", "/api/topics"].map((url) =>
+      fetch(url).then((response) => response.json())
+    );
+
+    Promise.all(urlsPromise).then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics,
+      });
+    });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
   }
 
   render() {
@@ -55,10 +90,9 @@ class Dashboard extends Component {
     ).map((panel) => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
-        value={panel.value}
-        onSelect={(event) => this.selectPanel(panel.id)}
+        value={panel.value(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
